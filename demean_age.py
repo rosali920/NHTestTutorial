64c6951d0f5a610f1fac9a28bc18871e11c9a157
Rosa Li <rosali920@gmail.com>
2018-07-30 16:58:25 -0700

    fixed importing for realsies I hope

--- demean_age.py
+++ demean_age.py
@@ -2,7 +2,8 @@ import sys
 import numpy as np
 import pandas as pd
 
-age = pd.read_csv(sys.argv[1], sep='\t')
+df = pd.read_csv(sys.argv[1], sep='\t')
+age=df.age
 
 mean_age = sum(age)/len(age)
 assert mean_age < 100
